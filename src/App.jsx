245c11afa71f0de7a74f8aff9407106586ba82c5
Lucas Duarte <lucasduarte.tec@gmail.com>
2025-09-20 15:15:03 -0300

    chore: add tailwind setup

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
 function App() {
-  return <h1>Hello World</h1>;
+  return <h1 className="text-red-500">Hello World</h1>;
 }
 export default App;
--- src/Tasks.jsx
+++
@@ -1,3 +0,0 @@
-function Tasks() {}
-
-export default Tasks;
