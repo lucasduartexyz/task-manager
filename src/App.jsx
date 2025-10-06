e7394dd8d08187be29c3e39ce783d0444e0f591c
duartetech <lucasduarte.tec@gmail.com>
2025-10-06 14:53:45 -0300

    feat: add Sidebar and SidebarButton component

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,10 @@
+import Sidebar from "./components/Sidebar"
+
 function App() {
-  return <h1 className="text-red-500">Hello World</h1>;
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  )
 }
-export default App;
+export default App
