7c73bd4d241fb9e2582e7d736d602947a9a6213e
duartetech <lucasduarte.tec@gmail.com>
2025-10-07 20:15:07 -0300

    feat: update spacing between sidebar and tasks

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -39,7 +39,7 @@ const Tasks = () => {
   }
   return (
     <div className="w-full px-8 py-16">
-      <div className="flex w-full justify-between">
+      <div className="flex w-full justify-between space-y-6">
         <div>
           <span className="text-xs font-semibold text-[#00ADB5]">
             Minhas Tarefas
