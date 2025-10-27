149ed824c97f7c816c3e53eb66cebbe77ef020e4
lucasduartexyz <lucasduarte.tec@gmail.com>
2025-10-26 21:53:17 -0300

    feat: add InputErrorMessage component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,5 +1,6 @@
 import { forwardRef } from "react"
 
+import InputErrorMessage from "./InputErrorMessage"
 import InputLabel from "./InputLabel"
 
 const Input = forwardRef(({ label, errorMessage, ...rest }, ref) => {
@@ -11,9 +12,7 @@ const Input = forwardRef(({ label, errorMessage, ...rest }, ref) => {
         className="rounded-lg border border-solid border-[#ECECEC] px-4 py-3 placeholder:text-sm placeholder:text-[#9A9C9F] focus:outline-[#00ad85] focus:outline-solid"
         ref={ref}
       />
-      {errorMessage && (
-        <p className="text-left text-xs text-red-500">{errorMessage}</p>
-      )}
+      {errorMessage && <InputErrorMessage>{errorMessage}</InputErrorMessage>}
     </div>
   )
 })
