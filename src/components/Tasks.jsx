82f6389b8a7957d074ff3b1864e24c6d2a55edf0
lucasduartexyz <lucasduarte.tec@gmail.com>
2025-11-07 13:22:42 -0300

    feat: ensure task is delete from db

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -35,9 +35,19 @@ const Tasks = () => {
   const afternoonTasks = tasks.filter((tasks) => tasks.time === "afternoon")
   const eveningTasks = tasks.filter((tasks) => tasks.time === "evening")
 
-  const handleTaskDeleteClick = (taskId) => {
-    const newTasks = tasks.filter((task) => task.id != taskId)
+  const handleTaskDeleteClick = async (taskId) => {
+    const response = await fetch(`http://localhost:3000/tasks/${taskId}`, {
+      method: "DELETE",
+    })
+
+    if (!response.ok) {
+      return toast.error(
+        "Erro ao deletar a tarefa. Por favor, tente novamente."
+      )
+    }
+    const newTasks = tasks.filter((task) => task.id !== taskId)
     setTasks(newTasks)
+
     toast.success("Tarefa deletada com sucesso!")
   }
 
@@ -77,6 +87,7 @@ const Tasks = () => {
         "Erro ao adicionar a tarefa. Por favor, tente novamente."
       )
     }
+    setTasks([...task, task])
     toast.success("Tarefa adiciona com sucesso!")
   }
   return (
